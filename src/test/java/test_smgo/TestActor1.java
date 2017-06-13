89bccd7a53a18c1a61a5c20cbe3c265b0cc58045
Julio Peralta <peraltajulioe@gmail.com>
2017-06-12 21:06:55 -0300

    test

--- src/test/java/test_smgo/TestActor1.java
+++ src/test/java/test_smgo/TestActor1.java
@@ -30,7 +30,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 @WebAppConfiguration
