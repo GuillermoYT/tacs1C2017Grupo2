6d5d9deee9d6e1c49dc281e459b4e5c2bf576ec6
Jon <joncolque@gmail.com>
2017-06-29 01:21:12 -0300

    avance con el test login todavia sin resolver.

--- src/test/java/tacs/LoginControllerTest.java
+++ src/test/java/tacs/LoginControllerTest.java
@@ -1,15 +1,31 @@
 package tacs;
 
 import org.junit.Before;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@WebAppConfiguration
 public class LoginControllerTest {
 
     private MockMvc mockMvc;
@@ -20,15 +36,21 @@ public class LoginControllerTest {
     }
     @Test
     public void createAuthenticationToken() throws Exception {
+        //String LOGIN_REQUEST = "{\"username\" : \"Jon\",\"password\" : \"1234\"}";
+        this.mockMvc.perform(formLogin("/auth").user("Guille").password("1234"))
+//          .andExpect(status().isOk())
+             .andDo(print());
 
-        MvcResult result = this.mockMvc.perform(post("/auth")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"username\":\"Guille\",\"password\":\"1234\"}"))
-                .andExpect(status().isOk())
-                .andReturn();
+    }
+    @Test
+    public void createAuthenticationToken2() throws Exception {
 
-        String content = result.getResponse().getContentAsString();
-        System.out.println(content);
+        this.mockMvc.perform(post("/auth").param("username","Jon").param("password","1234"))
+
+//                .andExpect(status().isOk())
+                .andDo(print());
+
+
     }
 
     @Test
